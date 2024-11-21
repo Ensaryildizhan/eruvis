a89fdac2311528d863cdd18336cb8bbedb3a565c
Ensar YILDIZHAN <architectoffical@gmail.com>
2024-11-21 19:11:16 +0300

    Kullanılmayan bileşenler kaldırıldı

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,5 +1,5 @@
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+// import { Analytics } from "@vercel/analytics/react";
+// import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Metadata } from 'next';
 import { ReactNode } from 'react';
 
